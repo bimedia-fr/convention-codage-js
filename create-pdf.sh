5d3776b83c282382851e100c14e618b8427e46c1
jerome creignou <jerome.creignou@bimedia.com.fr>
2015-06-10 11:53:32 +0200

    utilisation du script local

--- create-pdf.sh
+++ create-pdf.sh
@@ -1,1 +1,1 @@
-pandoc --template ~/.pandoc/templates/bimedia.latex -H ~/.pandoc/templates/header.latex -s  --toc --toc-depth 2 -o conventions-codage-js.pdf --highlight-style tango  conventions-codage-js.md
+create-page-pdf.sh conventions-codage-js.md
